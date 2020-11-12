eb75b926fa87d2d7c713bed983aa3aa351bcbceb
pureworldnew <pureworldnew@gmail.com>
2020-11-12 16:56:26 +0800

    update config for local

--- admin/config.php
+++ admin/config.php
@@ -11,20 +11,20 @@ define('HTTPS_CATALOG', 'https://test.gempacked.com/');
 // DIR
 define('QUANTITY','0');//0 for supplied and 1 for ordered
 
-define('DIR_APPLICATION', '/var/www/vhosts/test.gempacked.com/home/live/admin/');
-define('DIR_SYSTEM', '/var/www/vhosts/test.gempacked.com/home/live/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/test.gempacked.com/home/live/admin/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/test.gempacked.com/home/live/admin/view/template/');
-define('DIR_CONFIG', '/var/www/vhosts/test.gempacked.com/home/live/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/test.gempacked.com/home/live/image/');
-define('DIR_CACHE', '/var/www/vhosts/test.gempacked.com/home/live/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/test.gempacked.com/home/live/system/storage/download/');
-define('DIR_LOGS', '/var/www/vhosts/test.gempacked.com/home/live/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/test.gempacked.com/home/live/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/vhosts/test.gempacked.com/home/live/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/vhosts/test.gempacked.com/home/live/catalog/');
-define('DIR_ROUTE', '/var/www/vhosts/test.gempacked.com/home/live/');
-define('DIR_POS', '/var/www/vhosts/test.gempacked.com/home/live/pos/');
+define('DIR_APPLICATION', 'http://dev.opencart.com:8081/admin/');
+define('DIR_SYSTEM', 'http://dev.opencart.com:8081/system/');
+define('DIR_LANGUAGE', 'http://dev.opencart.com:8081/admin/language/');
+define('DIR_TEMPLATE', 'http://dev.opencart.com:8081/admin/view/template/');
+define('DIR_CONFIG', 'http://dev.opencart.com:8081/system/config/');
+define('DIR_IMAGE', 'http://dev.opencart.com:8081/image/');
+define('DIR_CACHE', 'http://dev.opencart.com:8081/system/storage/cache/');
+define('DIR_DOWNLOAD', 'http://dev.opencart.com:8081/system/storage/download/');
+define('DIR_LOGS', 'http://dev.opencart.com:8081/system/storage/logs/');
+define('DIR_MODIFICATION', 'http://dev.opencart.com:8081/system/storage/modification/');
+define('DIR_UPLOAD', 'http://dev.opencart.com:8081/system/storage/upload/');
+define('DIR_CATALOG', 'http://dev.opencart.com:8081/catalog/');
+define('DIR_ROUTE', 'http://dev.opencart.com:8081/');
+define('DIR_POS', 'http://dev.opencart.com:8081/pos/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
